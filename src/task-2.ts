c40dff84f5a9559bce68c7d57829da9bbd1cecfc
ig0r-84 <coshilnick@gmail.com>
2026-09-20 11:46:27 +0300

    add all tasks

--- src/task-2.ts
+++ src/task-2.ts
@@ -1,1 +1,12 @@
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+}
+const product: Product = {
+  id: 1,
+  title: "Tablet",
+  description: "Compact and fast",
+};
 
+console.log(`Product: ${JSON.stringify(product)}`);
